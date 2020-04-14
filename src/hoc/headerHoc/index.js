dd3f4ff23a8a70b11a3750e8d4434a98d5774052
Cryptonex7 <34197902+Cryptonex7@users.noreply.github.com>
2020-04-15 02:17:41 +0530

    Test Header Styles

--- src/hoc/headerHoc/index.js
+++ src/hoc/headerHoc/index.js
@@ -1,18 +1,41 @@
 import React from 'react';
+import {toggleTheme} from '../../services/actions';
+import IconButton from '@material-ui/core/IconButton';
+import BrightnessMedium from '@material-ui/icons/BrightnessMedium';
+import {connect} from 'react-redux';
+import {styles} from './styles';
+import withStyles from '@material-ui/core/styles/withStyles';
 
 const headerHoc = (WrapComponent) => {
   class HocContent extends React.Component {
     render() {
       return (
         <>
-          <div>---------------- header ---------------</div>
+          <div className={this.props.classes.root}>
+            ---------------- header ---------------
+            <IconButton color="primary" onClick={this.props.toggleTheme}>
+              <BrightnessMedium />
+            </IconButton>
+          </div>
 
           <WrapComponent />
         </>
       );
     }
   }
-  return HocContent;
+
+  const mapStateToProps = (state) => {};
+
+  const mapDispatchToProps = (dispatch) => {
+    return {
+      toggleTheme: () => dispatch(toggleTheme()),
+    };
+  };
+
+  return connect(
+    mapStateToProps,
+    mapDispatchToProps
+  )(withStyles(styles)(HocContent));
 };
 
 export default headerHoc;
---
+++ src/hoc/headerHoc/styles.js
@@ -0,0 +1,7 @@
+export const styles = (theme) => ({
+  root: {
+    background: theme.palette.background.header,
+    boxShadow: theme.palette.shadows.header,
+    color: theme.palette.text.primary,
+  },
+});
