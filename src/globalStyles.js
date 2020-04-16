c29191850c37c33c3c4ab951ef2a8fda406fb2d3
Cryptonex7 <34197902+Cryptonex7@users.noreply.github.com>
2020-04-16 20:09:37 +0530

    Added Global Styles

---
+++ src/globalStyles.js
@@ -0,0 +1,11 @@
+export const globalStyles = (theme) => ({
+  '@global': {
+    body: {
+      backgroundColor: theme.palette.background.default,
+    },
+    html: {
+      height: '100%',
+      backgroundColor: theme.palette.background.default,
+    },
+  },
+});
