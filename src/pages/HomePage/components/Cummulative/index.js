110d78d1b63bc183dca45aabdfcdc608ad06a557
Cryptonex7 <34197902+Cryptonex7@users.noreply.github.com>
2020-04-23 20:12:08 +0530

    Cummulative graphs to graph group

--- src/pages/HomePage/components/Cummulative/index.js
+++ src/pages/HomePage/components/Cummulative/index.js
@@ -1,22 +1,14 @@
-import React, {useState} from 'react';
+import React from 'react';
 import LineChart from './LineChart';
-import BinarySwitch from '../../../../modules/BinarySwitch';
 import {useStyles} from './styles';
 
 function Cummulative(props) {
-  const {title, data} = props;
+  const {title, data, logChecked} = props;
   const classes = useStyles();
-  const [logChecked, setLogChecked] = useState(false);
   return (
     <div className={classes.Cummulative}>
       <div className={classes.Title}>{title}</div>
       <LineChart data={data} logScale={logChecked} />
-      <BinarySwitch
-        leftVal="Linear"
-        rightVal="Logarithmic"
-        handleChange={setLogChecked}
-        rightChecked={logChecked}
-      />
     </div>
   );
 }
