44492c11afe8037b3a26fd98cbab6a347016fbe3
Cryptonex7 <34197902+Cryptonex7@users.noreply.github.com>
2020-05-07 15:06:08 +0530

    Quiz Container

--- src/pages/QuizPage/index.js
+++ src/pages/QuizPage/index.js
@@ -1,27 +1,18 @@
-import React, {useEffect} from 'react';
-import {useDispatch, useSelector} from 'react-redux';
+import React, {useEffect, useState} from 'react';
 import * as firebase from 'firebase/app';
 import 'firebase/firestore';
-
-import useTheme from '@material-ui/core/styles/useTheme';
-import useMediaQuery from '@material-ui/core/useMediaQuery';
-import Divider from '@material-ui/core/Divider';
 
 import LandingSection from './components/LandingSection';
 import TheQuiz from './components/TheQuiz';
 
 import {useStyles} from './styles';
 
-function HomePage() {
+function QuizPage() {
   const classes = useStyles();
-  const dispatch = useDispatch();
-  // const state = useSelector((state) => state);
-
-  const theme = useTheme();
-  const matches = useMediaQuery(theme.breakpoints.up('sm'));
-  const qlist = [];
+  const [qlist, setQlist] = useState([]);
 
   useEffect(() => {
+    const list = [];
     const fetchGraphData = async () => {
       firebase
         .firestore()
@@ -29,37 +20,21 @@ function HomePage() {
         .get()
         .then((querySnapshot) => {
           querySnapshot.forEach((doc) => {
-            qlist.push(doc.data());
+            list.push(doc.data());
           });
         })
-        .then(console.log(qlist));
+        .then(() => setQlist(list))
+        .then(() => console.log(list));
     };
     fetchGraphData();
   }, []);
-  // Object.keys(graphData).length > 0 ? (
+
   return true ? (
     <div className={classes.flexCol}>
       <div className={classes.root}>
         <div className={classes.compressor}>
-          {/* Header Section  */}
           <LandingSection />
-
-          {matches ? (
-            <>
-              <div className={classes.flexRow}>
-                <div className={classes.flexCol}>
-                  <TheQuiz qlist={qlist} />
-                </div>
-              </div>
-              <Divider light variant="middle" flexItem style={{height: 1}} />
-              <div className={classes.flexRow}>
-                With Love for Healthcare Workers, <br /> Team, <br />
-                CovidAnalyticsIndia &heart;
-              </div>
-            </>
-          ) : (
-            <></>
-          )}
+          <TheQuiz qlist={qlist} />
         </div>
       </div>
     </div>
@@ -68,4 +43,4 @@ function HomePage() {
   );
 }
 
-export default HomePage;
+export default QuizPage;
